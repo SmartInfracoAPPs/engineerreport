f9d474eccd039c148161b70975410bdd4e024f5f
SMARTINFRACO DEVS <157625837+SmartInfracoAPPs@users.noreply.github.com>
2024-06-29 02:21:46 +0000

    update app

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://api-server.197.253.124.146.sslip.io'),
 
     /*
     |--------------------------------------------------------------------------
